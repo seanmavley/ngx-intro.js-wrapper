fb21dcd7fbcfc7b0128adc260c9af459575e9d01
Rexford Nkansah <seanmavley@gmail.com>
2018-01-29 17:02:23 +0000

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,15 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
-// import { SampleComponent } from './sample.component';
-// import { SampleDirective } from './sample.directive';
-// import { SamplePipe } from './sample.pipe';
 import { NgxTourService } from './tour.service';
 
-// export * from './sample.component';
-// export * from './sample.directive';
-// export * from './sample.pipe';
 export * from './tour.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [
-    // SampleComponent,
-    // SampleDirective,
-    // SamplePipe
-  ],
-  exports: [
-    // SampleComponent,
-    // SampleDirective,
-    // SamplePipe
-  ]
+  declarations: [ ],
+  exports: [ ]
 })
 export class NgxIntroJsService {
   static forRoot(): ModuleWithProviders {
